13d9eb5df0308851aa24f2836b3ce82b9e3ea715
syousafz <153879678+syousafz@users.noreply.github.com>
2024-01-04 16:21:02 -0500

    Edited css

--- templates/script.js
+++ templates/script.js
@@ -11,5 +11,38 @@ function startUpdateCycle() : void {
             updatePrices();
             counter = 10;
         }
-    })
+    }, timeout : 1000)
+}
+
+$(document).ready(function () {
+    tickers.forEach(function(ticker : T) {
+        addTickerToGrid();
+    });
+
+    updatePrices();
+
+    $('add-ticker-form').submit(function(e) {
+        e.preventDefault();
+        var newTicker = $('#new-ticker').val().toUpperCase();
+        if (!tickers.includes(newTicker)) {
+            tickers.push(newTicker);
+            localStorage.setItem('tickers', JSON.stringify(tickers))
+            addTickerToGrid(newTicker);
+        }
+        $('new-ticker').val('');
+        updatePrices();
+    });
+
+    $('#tickers-grid').on('click', '.remove-btn', function() {
+        var tickerToRemove = $(this).data('ticker');
+        tickers = tickers.filter(t => t !== tickerToRemove);
+        localStorage.setItem('tickers', JSON.stringify(tickers))
+        $(`#${tickerToRemove}`).remove();
+    });
+
+    startUpdateCycle();
+});
+
+function addTickerToGrid(ticker): void {
+    $('#ticker-grid').append(`<div id="${ticker}" class="stock-box">`)
 }
